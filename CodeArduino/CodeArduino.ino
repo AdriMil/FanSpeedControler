c74558b6193fd93b6de9610c487d9ff82b4e17f1
AdriMil <adrien.millot@yahoo.fr>
2024-01-20 23:44:47 +0100

    :sparkles: Variation de la vitesse + Led d'indications

--- CodeArduino/CodeArduino.ino
+++ CodeArduino/CodeArduino.ino
@@ -1,10 +1,47 @@
-const int pinPWM = 11; // Broche PWM D11 sur Arduino Nano
+const int pinButton = 5; 
+const int LedVerte = 8; 
+const int LedRouge = 7;
+const int LedJaune = 6; 
+const int pinPWM = 11;   
+int pwmValues[] = {255, 150, 0};  //3 vitesses definies pr le ventilateur
+int iterator = 0; 
+int buttonState = HIGH; 
 
 void setup() {
-  // Aucune initialisation nécessaire dans cet exemple
+  pinMode(pinButton, INPUT);
+  pinMode(pinPWM, OUTPUT);
+  pinMode(LedVerte, OUTPUT);
+  pinMode(LedRouge, OUTPUT);
+  pinMode(LedJaune, OUTPUT);
+  
+  analogWrite(pinPWM, pwmValues[iterator]); //Vitesse maximum à l'allumage
+  digitalWrite(LedRouge, HIGH);digitalWrite(LedVerte, LOW);digitalWrite(LedJaune, HIGH); //Led verte On à l'allumage
+  Serial.begin(9600);
 }
 
 void loop() {
-  // Générer un signal PWM à un niveau de 100 sur la broche D11
-  analogWrite(pinPWM, 100);
+  buttonState = digitalRead(pinButton); //Lecture de l'état du bouton
+
+  //Changement de vitesse à chaque appuis
+  if (buttonState == HIGH) { 
+    if (iterator == 2) {
+      iterator = 0;
+    } else {
+      iterator++;
+    }
+
+  //Allumage des LEDs en fonction de la vitesse du ventilateur
+  if(iterator == 2){ digitalWrite(LedRouge, LOW);digitalWrite(LedVerte, HIGH);digitalWrite(LedJaune, HIGH);}
+  else if(iterator == 1){ digitalWrite(LedRouge, HIGH);digitalWrite(LedVerte, HIGH);digitalWrite(LedJaune, LOW);}
+  else { digitalWrite(LedRouge, HIGH);digitalWrite(LedVerte, LOW);digitalWrite(LedJaune, HIGH);}
+      
+  analogWrite(pinPWM, pwmValues[iterator]);
+  delay(500);  // Ajout d'un délai pour éviter les rebonds du bouton
+  }
+
+
+
+
+
+  
 }
