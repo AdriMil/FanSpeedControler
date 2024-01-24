7b1868e5050b75db5e746ae061e77afe664f780b
Adrien Millot <adrien.millot@avisto.com>
2024-01-24 12:56:10 +0100

    🚧 test precommit action

--- CodeArduino/CodeArduino.ino
+++ CodeArduino/CodeArduino.ino
@@ -15,10 +15,15 @@ void setup() {
   pinMode(LedJaune, OUTPUT);
 
   analogWrite(pinPWM, pwmValues[iterator]); // Vitesse maximum à l'allumage
+
+
   digitalWrite(LedRouge, HIGH);
+
+
+
   digitalWrite(LedVerte, LOW);
   digitalWrite(LedJaune, HIGH); // Led verte On à l'allumage
-  Serial.begin(9600);
+Serial.begin(9600);
 }
 
 void loop() {
